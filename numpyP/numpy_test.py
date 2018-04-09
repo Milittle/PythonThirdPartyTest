76b48d1f4320bc149950727729cade766ff41dd7
milittle <mizeshuang@gmail.com>
2018-04-10 00:02:24 +0800

    20180410

--- numpyP/numpy_test.py
+++ numpyP/numpy_test.py
@@ -8,6 +8,8 @@
 
 import numpy as np
 from numpy import linalg
+import random
+
 
 
 # 创建数组
@@ -47,36 +49,49 @@ def basic_operation():
     print(np.log2(2))
     print(np.max([1, 2, 3, 4]))
 
-
+def cal_svd(A):
+    U, E, V = linalg.svd(A)
+    return U,E,V
 
 
 A = np.floor(np.random.randn(2,3) * 10)
-print('A:\n', A)
+# print('A:\n', A)
 B = np.floor(np.random.randn(2,3) * 10)
-print('B:\n', B)
+# print('B:\n', B)
 C = np.floor(np.random.randn(2,3) * 10)
-print('C:\n', C)
+# print('C:\n', C)
 
 
+def matrix_mul():
+    A = np.array([[1, 2], [-1, 4]])
+    B = np.array([[2, 0], [3, 4]])
+    print('对应元素相乘：', A * B)
+    print('矩阵乘法:', np.dot(A, B))
+    # or print(A.dot(B))
 
-A = np.array([[1, 2], [-1, 4]])
-B = np.array([[2, 0], [3, 4]])
-print('对应元素想乘：', A * B)
-print('矩阵乘法:', np.dot(A, B))
-# or print(A.dot(B))
+def matrix_tr_inv_eig():
+    # 求A的转置
+    print ('A的转置:', A.transpose())
+    # 求A的逆矩阵
+    print('A的逆矩阵：', linalg.inv(A))
+    # 特征值和特征向量
+    eigenvalues, eigenvectors = linalg.eig(A)
+    print('A 的特征值：', eigenvalues)
+    print('A 的特征向量：', eigenvectors)
 
+A_svd = np.array([[2,4],[1,3],[0,0],[0,0]])
 
-# 求A的转置
-print ('A的转置:', A.transpose())
-# 求A的逆矩阵
-print('A的逆矩阵：', linalg.inv(A))
-# 特征值和特征向量
-eigenvalues, eigenvectors = linalg.eig(A)
-print('A 的特征值：', eigenvalues)
-print('A 的特征向量：', eigenvectors)
+def sample_data_random():
+    a = random.sample(range(0, 15), 8)
+    b = random.randint(0, 65)
+    print(a)
+    print(b)
 
 if __name__ == '__main__':
-    basic_operation()
+    U, E, V = cal_svd(A_svd)
+    print(U)
+    print(E)
+    print(V)
 
 
 
